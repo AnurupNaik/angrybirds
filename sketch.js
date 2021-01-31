79c3ddff6f81f6e5028e1290875eaf52bf1e9660
AnurupNaik <anurup.naik@starnoldschool.in>
2021-01-31 13:19:09 +0530

    files have been added

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var  chain
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 =  new Log(200,100,100, PI/2);
+    chain = new Chain(bird.body,log6.body)
 }
 
 function draw(){
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
 }
